e794b925b0462defe3c0183f1b540e83a8ee46cd
alvar <alvaro.esqui@hotmail.com>
2024-10-09 20:02:10 -0600

    Add XGBoost and Catboost

--- models.py
+++ models.py
@@ -6,11 +6,16 @@ def rf():
 def cb():
     print('Ejecutando Catboost')
 
+def xg():
+    print('Ejecutando XGBoost')
+
 def main():
     if sys.argv[1] == 'rf':
         rf()
     elif sys.argv[1] == 'cb':
         cb()
+    elif sys.argv[1] == 'xg':
+        xg()
     else:
         print("Sin modelo")
 
