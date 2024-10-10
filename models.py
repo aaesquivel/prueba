c4caec8485173a8a080f29a28dd7997308bd94a5
alvar <alvaro.esqui@hotmail.com>
2024-10-09 19:41:46 -0600

    Add models.py for prediction

---
+++ models.py
@@ -0,0 +1,13 @@
+import sys
+
+def rf():
+    print('Ejecutando Random Forest')
+
+def main():
+    if sys.argv[1] == 'rf':
+        rf()
+    else:
+        print("Sin modelo")
+
+if __name__ == '__main__':
+    main()
